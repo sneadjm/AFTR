0fa85477c0148afed58d561c230d8cfdd7e7d947
jsnead3 <jennasnead@gmail.com>
2025-07-27 23:17:09 -0400

    More messy containers than a tupperware drawer

--- aftr-python/app/database.py
+++ aftr-python/app/database.py
@@ -8,6 +8,8 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+print("DB URL loaded from env:", os.getenv("DATABASE_URL"))
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
