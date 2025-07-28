3b66d9799091577bf1778d8e7d2ccbf8abdb15d8
jsnead3 <jennasnead@gmail.com>
2025-07-28 00:29:33 -0400

    Frontend login page fixes

--- aftr-python/app/database.py
+++ aftr-python/app/database.py
@@ -8,8 +8,6 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-print("DB URL loaded from env:", os.getenv("DATABASE_URL"))
-
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
