2c1effb1c53eb0b6e906a52c1e5b5db45c52e3cf
jsnead3 <jennasnead@gmail.com>
2025-08-17 21:50:31 -0400

    User registry fixes

--- aftr-python/app/main.py
+++ aftr-python/app/main.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+import os
 load_dotenv()
 
 from fastapi import FastAPI
@@ -6,10 +7,21 @@ from app.routers import users, friends, photos
 from app.database import engine
 import app.models as models
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+FRONTEND_URL = os.getenv('FRONTEND_URL')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[FRONTEND_URL],  
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],  
+)
 
 @app.get("/")
 def read_root():
